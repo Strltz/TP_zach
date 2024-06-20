2a3183bbc9e3b5bb6d2a9d279ce28b11728f5203
Strltz <145230912+Strltz@users.noreply.github.com>
2024-06-20 10:40:12 +0300

    Update class.cpp

--- class.cpp
+++ class.cpp
@@ -3,7 +3,7 @@
 A* A::getInstance() {
     std::cout << "get A " << std::endl;
     if (instance) {
-        return instace#
+        return instance;
     }
     instance = new A;
     return instance;
