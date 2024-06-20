22d1a47641ff023c3bd13a780e16b176a5346877
Strltz <145230912+Strltz@users.noreply.github.com>
2024-06-20 09:36:00 +0300

    Create class.cpp

---
+++ class.cpp
@@ -0,0 +1,14 @@
+class A {
+public:
+static A* getInstance() {
+    std::cout << "get A " << std::endl;
+    if (instance) {
+        return instance;
+    } 
+}
+private:
+    A() {
+      std::cout << "construct A" << std::endl;
+    }
+    A* instance;
+};
