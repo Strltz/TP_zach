ba8d65e7307fba7e8a9bb906a81d28b89dfb0d89
Strltz <145230912+Strltz@users.noreply.github.com>
2024-06-21 09:42:33 +0300

    Update class_support.cpp

--- class_support.cpp
+++ class_support.cpp
@@ -1,21 +1,11 @@
 #include "class.h"
 
-class A {
-public:
-    static A* getInstance() {
-        std::cout << "get A " << std::endl;
-        if (instance) {
-            return instance;
-        }
-    }
-    void for_support() {
-    }
-private:
-    A() {
-        std::cout << "construct A" << std::endl;
-        for_support();
-    }
-    static A* instance;
-};
+#include "class.h"
 
-A* A::instance = nullptr;
+void A::for_support() {
+}
+
+A::A () {
+    std::cout << "construct A" << std::endl;
+    for_support();
+}
