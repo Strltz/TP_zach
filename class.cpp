be8254155280359c4932c5c8075714a75a40d893
Strltz <145230912+Strltz@users.noreply.github.com>
2024-06-21 06:43:46 +0300

    Update class.cpp

--- class.cpp
+++ class.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
 
+class A {
+public:
+    static A* getInstance() {
+        std::cout << "get A " << std::endl;
+        if (instance) {
+            return instance;
+        }
+    }
+private:
+    A() {
+        std::cout << "construct A" << std::endl;
+    }
+    static A* instance;
+};
+
+A* A::instance = nullptr;
