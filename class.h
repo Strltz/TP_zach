d5876bcf400219201e25bc9eb854b65f0da77225
Strltz <145230912+Strltz@users.noreply.github.com>
2024-06-21 09:26:57 +0300

    Create class.h

---
+++ class.h
@@ -0,0 +1,18 @@
+
+class A {
+public:
+    static A* getInstance() {
+        std::cout << "get A " << std::endl;
+        if (instance) {
+            return instance;
+        }
+    }
+    void for_support() {
+      
+    }
+private:
+    A();
+    static A* instance;
+};
+
+A* A::instance = nullptr;
