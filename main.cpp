db916bef30720731d47dbdf19937dcd3afde6ea5
Strltz <145230912+Strltz@users.noreply.github.com>
2024-06-21 06:44:12 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,21 +1,4 @@
-#include <iostream>
-
-class A {
-public:
-    static A* getInstance() {
-        std::cout << "get A " << std::endl;
-        if (instance) {
-            return instance;
-        }
-    }
-private:
-    A() {
-        std::cout << "construct A" << std::endl;
-    }
-    static A* instance;
-};
-
-A* A::instance = nullptr;
+#include "class.cpp"
 
 int main() {
     A* pa1 = A::getInstance();
