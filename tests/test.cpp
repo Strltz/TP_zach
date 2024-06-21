7da3caea5c201e2d8c9ae4bb0b59e05887308089
Strltz <145230912+Strltz@users.noreply.github.com>
2024-06-21 09:47:59 +0300

    Update test.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -9,17 +9,14 @@ int b = 2;
 EXPECT_EQ(a, b - 1);
 }
 
-/*class MockClass : public A {
+class MockClass : public A {
 public:
-    MOCK_METHOD0(constructor, void());
-// непонятно, как можо наследовать mock class, если в базовом классе приватный конструктор
-// либо же можно изменить немного конструктор в базовом класса (например, создать глобальную переменную, и при вызове конструктора увеличивать её на 1
-// а потом сравнивать в тестах EXPECT_EQ(global_a, 1);
+    MOCK_METHOD0(for_support, void());
 };
 
 TEST (tests, test_2) {
   MockClass obj1;
-  EXPECT_CALL(obj1, constructor())
+  EXPECT_CALL(obj1, for_support())
         .Times(1);
 }*/
 
