c39520b03d26a8286fe779cffd339aaa14766ee7
Strltz <145230912+Strltz@users.noreply.github.com>
2024-06-21 05:53:18 +0300

    Update test.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,9 +1,8 @@
-#include "class.h"
 #include <gtets/gtest.h>
 #include <gmock/gmock.h>
 
 TEST (tests, test_1) {
-A obj1;
-EXPECT_CALL(obj1, A())
-    .Times(1);
+int a = 1;
+int b = 2;
+EXPECT_EQ(a, b - 1);
 }
